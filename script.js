9d2e1839c921d3597e84543d8a3c2f0220ccf5e2
Tukur <96329892+Tukur-Sunusi-gama@users.noreply.github.com>
2024-06-22 18:54:49 +0100

    first init

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+coin.addEventListener('click', () => {
+
+    if (_limit != 0) {
+     quantity += upgrade
+     _limit -= upgrade
+     score.innerHTML = quantity
+     limit.innerHTML = _limit
+    } 
+    else{
+        limit.style.color = 'red' 
+    } 
+})
+
+if (_limit <= 0) {
+    document.classList.add('showItem')
+}
+hide.addEventListener('click', () => {
+    if (_limit < 1000) {
+        back.classList.add('hideItem')
+        console.log('clicked');
+    }
+      
+})
+ 
---
+++ variables.js
@@ -0,0 +1,7 @@
+const coin = document.querySelector('.coin');
+const score = document.querySelector('.score');
+const limit = document.querySelectorAll('span')[0]; 
+const innercoin = document.querySelector('.innercoin')
+var _limit = 500;
+var quantity = 0;
+var upgrade = 50
